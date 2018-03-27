51b3380779585f512bb3edaf2654b5b4394cbca5
nhan1906 <nhantx.1906@gmail.com>
2018-03-27 10:06:17 +0700

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'core/init.php';
+include 'includes/meta.php';
+?>
+
+<!-- Style for this page here -->
+</head>
+<body>
+
+	<!-- Navigation -->
+	<!-- Show product-->
+	<!-- Modal -->
+	<!-- Contact -->
+	<!-- Footer -->
+
+
+	<!-- Bottom pages -->
+	<script src="js/javascript.js"></script>
+</body>
+</html>
